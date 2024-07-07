7bac4fadfd2093648d87d443d7856d1ea87e5559
IvonneDare <IPN.IVONNE@HOTMAIL.COM>
2024-07-07 22:28:52 +0000

    Changes to be committed: 	modified:   Strings.js

--- Strings.js
+++ Strings.js
@@ -6,16 +6,28 @@ const Terceraopc = 'Comillas simples'
 // concadenar 
 const direccion = 'calle dalsa123'
 const ciudad = 'Iztapalapa'
-const direccioncompleta  = 'mi direccion completa es ' + direccion + '' + ciudad 
+const direccioncompleta  = 'mi direccion completa es: ' + direccion + ' ' + ciudad 
 console.log(direccioncompleta)
 
+//concadenacion2 
 
+const nombre = 'Ivonne'
+const Pais = '🇲🇽'
+const Presentacion = 'Hola, soy ${nombre} de ${pais}'
 
+console.log(Presentacion)
 
+// tercera concadenacion por medio de array 
+
+const primera = 'ME encanta '
+const segunda = 'la gente'
+const tercera = '🇲🇽'
+const pensamiento = [primera, segunda, tercera]
+console.log (pensamiento.join(''))
 
 
 //concadenacion 
-//string: literals y values 
+//string: literals y values = 
 //caracteres de escape 
 //string largos 
 //metodos 
