779867b4498fe0a287148e4c76d2da274c1fd8e5
IvonneDare <IPN.IVONNE@HOTMAIL.COM>
2024-07-07 22:10:03 +0000

    Changes to be committed: 	new file:   Strings.js

---
+++ Strings.js
@@ -0,0 +1,22 @@
+//Creacion de string 
+const primperaopc = 'Comillas simples'
+const Segundaopc = "Comilla simple"
+const Terceraopc = 'Comillas simples'
+
+// concadenar 
+const direccion = 'calle dalsa123'
+const ciudad = 'Iztapalapa'
+const direccioncompleta  = 'mi direccion completa es ' + direccion + '' + ciudad 
+console.log(direccioncompleta)
+
+
+
+
+
+
+//concadenacion 
+//string: literals y values 
+//caracteres de escape 
+//string largos 
+//metodos 
+
