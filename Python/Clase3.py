c2ec9175d40fc54f85b50f8dc47022475778e7e4
IvonneDare <IPN.IVONNE@HOTMAIL.COM>
2024-07-20 21:43:31 +0000

    Changes to be committed: 	new file:   Python/Clase3.py     	new file:   Python/Prueba.py

---
+++ Python/Clase3.py
@@ -0,0 +1,26 @@
+saludo = "hola Pelusita"
+nombre = "Dare"
+saludo_2 = "Hola Ivonne" 
+Caracter = "C"
+saludo_3 = 'Hola priciosa Pelusa'
+saludo_4 ='''Saltos de lienas 
+last_name = "Rodriguez" 
+
+en 
+
+los 
+
+string'''
+
+print (saludo)
+print (saludo_2)
+print (nombre)
+print ("Pelusa linda")
+
+name = "Ivonne"
+print (type(name))
+print (type(Caracter))
+print (saludo_4)
+print (nombre[1])
+print (nombre[-3])
+
---
+++ Python/Prueba.py
@@ -0,0 +1,27 @@
+
+name = "Pelusa R"
+last_name = "Rodriguez    I"
+frase = "nunca pares de aprender"
+author = "Paltzi"
+Valor = 3.14159
+valor = 123.456789
+
+print ("nunca", "pares",sep=";")
+print ("Hola soy \'Ivonne\'")
+print ("Hola\n mundo")
+print("{:.2f}".format(valor))  # Imprime el valor con 2 decimales: 123.46
+print ("Valor: {:.2f}".format(Valor))
+print ("frase:", frase, "Autor", author)
+print (f"frase: {frase}, Autor:{author}")
+print ("Frase:{}, Autor:{}".format(frase,author))
+print ("Hola mundo")
+print (1+5)
+print (name [-2])
+
+print (name * 5 )
+print (name + " "+ last_name)
+print (len (last_name))
+print (len (name))
+print (name.lower())
+print (name.upper())
+print (last_name.strip())
